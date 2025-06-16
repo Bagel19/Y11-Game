0f32a70bd72f5a69bed5ef5a65c6fcc2c872e79c
Basil <seagar16@kingshigh.school.nz>
2025-06-16 14:59:22 +1200

    2.3
    I did nothing important. Tried to work on the timer but it didn't work. Fixed the buttons UI and I was about to work on a pause button.

--- y11-game/Scripts/HardWorld.gd
+++ y11-game/Scripts/HardWorld.gd
@@ -1,26 +1,5 @@
 extends Node2D
 
-var seconds = 0
-var minutes = 0
-var Dseconds = 0
-var Dminutes = 2
-
 func _on_endof_level_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		get_tree().change_scene_to_file("res://Scenes/Victory.tscn")
-
-func _on_timer_timeout():
-	if seconds == 0:
-		if minutes > 0:
-			minutes -= 1
-
-$Label.text=String(minutes)+":"+String(seconds)
-pass
-
-func Reset_Timer():
-	seconds=Dseconds
-	minutes=Dminutes
-
-#func _on_endof_level_body_entered(body: Node2D) -> void:
-	#if body.is_in_group("Player"):
-		#get_tree().change_scene_to_file("res://Scenes/Victory.tscn")
---
+++ y11-game/Scripts/Timer.gd
@@ -0,0 +1,31 @@
+extends Control
+
+var seconds = 0
+var minutes = 0
+var Dseconds = 0
+var Dminutes = 2
+
+func _ready():
+	Reset_Timer()
+	update_label()
+	$Timer.start()
+
+func _on_Timer_timeout() -> void:
+	if seconds == 0:
+		if minutes > 0:
+			minutes -= 1
+			seconds = 59
+		else:
+			$Timer.stop()
+			return
+	else:
+		seconds -= 1
+
+	update_label()
+
+func update_label():
+	$Label.text = str(minutes).pad_zeros(2) + ":" + str(seconds).pad_zeros(2)
+
+func Reset_Timer():
+	seconds = Dseconds
+	minutes = Dminutes
