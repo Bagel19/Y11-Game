1a0666e07c84e37dfd4ed4ed56e5b41e49a09dd1
Basil <seagar16@kingshigh.school.nz>
2025-08-14 09:57:38 +1200

    4.2
    All I did was upload the game into itch.io, I saw a bug in the pause menu, so I'm trying to fix that now.

--- y11-game/Scripts/Difficulty Screen.gd
+++ y11-game/Scripts/Difficulty Screen.gd
@@ -5,3 +5,6 @@ func _on_easy_mode_pressed() -> void:
 
 func _on_hard_mode_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/HardWorld.tscn")
+
+func _on_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
--- y11-game/Scripts/Pause.gd
+++ y11-game/Scripts/Pause.gd
@@ -25,6 +25,7 @@ func _on_restart_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Difficulty Screen.tscn")
+	resume()
 
 func _process(delta: float) -> void:
 	Esc()
