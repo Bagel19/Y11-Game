338a45863c9fb0a14b6bf8c0fe0bb0e1939a2682
Basil <seagar16@kingshigh.school.nz>
2025-08-15 10:58:35 +1200

    4.3
    As I said earlier, I managed to fix the pause menu completely. I will try to work on the powerups now.

--- y11-game/Scripts/Difficulty Screen.gd
+++ y11-game/Scripts/Difficulty Screen.gd
@@ -1,4 +1,6 @@
 extends Control
+func _ready() -> void:
+	get_tree().paused = false
 
 func _on_easy_mode_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/EasyWorld.tscn")
--- y11-game/Scripts/Pause.gd
+++ y11-game/Scripts/Pause.gd
@@ -1,7 +1,9 @@
 extends Control
 
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	$AnimationPlayer.play("RESET")
+
 func resume():
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("Blur")
@@ -11,10 +13,11 @@ func pause():
 	$AnimationPlayer.play("Blur")
 
 func Esc():
-	if Input.is_action_just_pressed("Pause") and get_tree().paused == false:
-		pause()
-	elif Input.is_action_just_pressed("Pause") and get_tree().paused == true:
-		resume()
+	if Input.is_action_just_pressed("Pause"):
+		if get_tree().paused:
+			resume()
+		else:
+			pause()
 
 func _on_resume_pressed() -> void:
 	resume()
@@ -24,8 +27,8 @@ func _on_restart_pressed() -> void:
 	get_tree().reload_current_scene()
 
 func _on_quit_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/Difficulty Screen.tscn")
-	resume()
 
 func _process(delta: float) -> void:
 	Esc()
